ef402ed17019ca22e43fd231b716157e5f791eb2
Juan Camilo Prada <juankprada@gmail.com>
2024-02-27 16:42:07 +0900

    Deleted most of the contentin this repo as is not needed. It should be generated from the base config

--- shared/home/dot-emacs.d/elpa/scopeline-20231027.1524/scopeline-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "scopeline" "20231027.1524" "Show scope info of blocks in buffer at end of scope"
-  '((emacs "26.1"))
-  :commit "58d6ef20b6cf398c48571239311d812a2f926ecb" :keywords
-  '("scope" "context" "tree-sitter" "convenience")
-  :url "https://github.com/meain/scopeline.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
