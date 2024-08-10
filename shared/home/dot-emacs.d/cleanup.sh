80af24806320aac41c1c1d800df0152c9e3121fb
Juan Camilo Prada <juankprada@gmail.com>
2024-08-10 17:11:38 +0900

    Added a cleanup script for emacs directory

---
+++ shared/home/dot-emacs.d/cleanup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rm -rf auto-save-alist
+rm -rf eln-cache
+rm -rf elpa
+rm -rf snippets
+rm -rf transient
+rm -rf tree-sitter
+
+rm -f juank-emacs-modules/*.el
+
+rm -f early-init.el 
+rm -f custom-set-variables.el
+rm -f init.el
+
+rm -f recentf
+rm -f org-roam.db 
